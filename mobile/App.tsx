8957f2128255acb09e8ef52106526eb4fc8fb4f3
Israel Nazareth <israel.fab@hotmail.com>
2022-11-17 18:34:06 -0300

    feat: initial config for react-native

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+import { Text } from './src/components/Text';
+
+export default function App() {
+  useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  return (
+    <View style={styles.container}>
+
+
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
