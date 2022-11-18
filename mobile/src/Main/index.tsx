71a2f6c1025096bae1f548bbc1f5d617677bdf1d
Israel Nazareth <israel.fab@hotmail.com>
2022-11-18 00:47:31 -0300

    style: finishes TableModal component

--- mobile/src/Main/index.tsx
+++ mobile/src/Main/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
+import { TableModal } from '../components/TableModal';
 import {
   Container,
   CategoriesContainer,
@@ -11,6 +13,13 @@ import {
 } from './styles';
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -28,11 +37,19 @@ export function Main() {
 
       <Footer>
         <FooterContainer>
-          <Button onPress={() => alert('Pressionou!')}>
-            Novo Pedido
-          </Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          )}
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
